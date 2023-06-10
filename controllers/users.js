3e466256785bcd76ac9ac3731d80aba67f46e113
Alina Sultanova <sultanova1859@gmail.com>
2023-06-10 09:36:14 +0500

    check

--- controllers/users.js
+++ controllers/users.js
@@ -13,9 +13,13 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
 
-  User.create({ name, about, avatar })
+  User.create({
+    name, about, avatar, email, password,
+  })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
