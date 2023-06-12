11874998ebf51861776168178fd530c3977c67c3
Alina Sultanova <sultanova1859@gmail.com>
2023-06-12 16:14:06 +0500

    added limiter

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const rateLimit = require('express-rate-limit');
 const mainRouter = require('./routes');
 const { REFERENCE_ERROR } = require('./errors/errorsCodes');
 
@@ -9,12 +10,22 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 app.use(express.json());
 app.use(express.urlencoded());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(cookieParser());
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 
 app.use(mainRouter);
 
