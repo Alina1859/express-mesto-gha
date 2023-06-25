626d5b7701cf469e7943e5ed89f1a473670b93b4
Alina Sultanova <sultanova1859@gmail.com>
2023-06-25 10:51:24 +0500

    fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,7 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
   'https://fronten.domain.nomoreparties.sbs',
+  'https://api.domainname.students.nomoreparties.sbs',
   'localhost:3000',
 ];
 
